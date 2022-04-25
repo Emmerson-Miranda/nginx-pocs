bc8105d3f6ec08bd24f4b778dab3a9fd83616204
Emmerson <emmerson.miranda@gmail.com>
2022-04-26 00:55:49 +0100

    changing pki role and comment command on templates

--- nginx-docker-consul-template/README.md
+++ nginx-docker-consul-template/README.md
@@ -1,3 +1,7 @@
 # NGINX-DOCKER-CONSUL-TEMPLATE
 
 This is a POC that integrate NGINX with Consul Template. Consul template is pulling and renewing certificates from  Hashicorp Vault.
+
+https://github.com/hashicorp/consul-template/blob/main/examples/vault-pki.md
+
+
--- nginx-docker-consul-template/docker/opt/poc/start.sh
+++ nginx-docker-consul-template/docker/opt/poc/start.sh
@@ -7,7 +7,7 @@ if [ "$ENABLE_CONSUL_TEMPLATE" == "true" ]
 then
     echo "Starting NGINX with Consul template"
     #service nginx start & consul-template -template="/templates/service.ctmpl:/etc/nginx/conf.d/service.conf:service nginx reload"
-    service nginx start & consul-template -config "/opt/poc/consul-template/template.hcl"
+    nginx -g 'daemon off;' & consul-template -config "/opt/poc/consul-template/template.hcl" 
 else
     echo "Starting NGINX without Consul template because ENABLE_CONSUL_TEMPLATE is $ENABLE_CONSUL_TEMPLATE instead of  'true'."
     #service nginx start 
